93e53aaa6cd4db7c3b53c8fbae02af1e790f544a
Johann <johann@dotgee.fr>
2012-10-31 11:16:05 +0100

    Add businessimmo to providers

--- lib/spree_social/engine.rb
+++ lib/spree_social/engine.rb
@@ -2,7 +2,8 @@ module SpreeSocial
   OAUTH_PROVIDERS = [
     ["Facebook", "facebook"],
     ["Twitter", "twitter"],
-    ["Github", "github"]
+    ["Github", "github"],
+		["Businessimmo", "businessimmo"]
   ]
 
   class Engine < Rails::Engine
